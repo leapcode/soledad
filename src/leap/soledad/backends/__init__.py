92724a1bf110a6f97dbfaec4a7d68388eef19dc5
Tomás Touceda <chiiph@leap.se>
2013-06-07 12:56:34 -0300

    Do not list the backends as __all__

--- src/leap/soledad/backends/__init__.py
+++ src/leap/soledad/backends/__init__.py
@@ -19,18 +19,3 @@
 """
 Backends that extend U1DB functionality.
 """
-
-from leap.soledad.backends import (
-    objectstore,
-    couch,
-    sqlcipher,
-    leap_backend,
-)
-
-
-__all__ = [
-    'objectstore',
-    'couch',
-    'sqlcipher',
-    'leap_backend',
-]
