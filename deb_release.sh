75a88bdb726425802f99c78f153ddde845cb9f1a
drebs <drebs@leap.se>
2014-06-06 16:51:38 -0300

    Shorten and fix deb_release.sh.

--- deb_release.sh
+++ deb_release.sh
@@ -1,25 +1,13 @@
 #!/bin/zsh
 
-cd common
-echo `pwd`
-rm src/leap/soledad/common/_version.py
-python setup.py freeze_debianver
-sed -i 's/-dirty//g' src/leap/soledad/common/_version.py 
-cd ..
+for pkg in common client server; do
+  cd ${pkg}
+  echo `pwd`
+  rm src/leap/soledad/${pkg}/_version.py
+  python setup.py freeze_debianver
+  sed -i 's/-dirty//g' src/leap/soledad/${pkg}/_version.py 
+  git add src/leap/soledad/${pkg}/_version.py
+  cd ..
+done
 
-cd client
-rm src/leap/soledad/client/_version.py
-python setup.py freeze_debianver
-sed -i 's/-dirty//g' src/leap/soledad/client/_version.py 
-cd ..
-
-cd server
-rm src/leap/soledad/server/_version.py
-python setup.py freeze_debianver
-sed -i 's/-dirty//g' src/leap/soledad/server/_version.py 
-cd ..
-
-git add common/src/leap/soledad/common/_version.py
-git add client/src/leap/soledad/client/_version.py
-git add server/src/leap/soledad/server/_version.py
-git ci -m "freeze debian version"	
+git commit -m "Freeze debian version."
