leapcode/soledad

commit aafa79c0f5e3d05c28d8f41804ae692931e67d7e

fix ssl negotiation
since ssl.SSLContext does not exist prior to python 2.7.9

client/src/leap/soledad/client/__init__.py
@@ -809,22 +809,25 @@ class VerifiedHTTPSConnection(httplib.HTTPSConnection):
             self.sock = sock
             self._tunnel()
 
-        # negotiate the best availabe version...
-        ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        highest_supported = ssl.PROTOCOL_SSLv23
 
-        # but if possible, we want to disable bad ones
-        # needs python 2.7.9+
         try:
+            # needs python 2.7.9+
+            # negotiate the best available version,
+            # but explicitely disabled bad ones.
+            ctx = ssl.SSLContext(highest_supported)
             ctx.options |= ssl.OP_NO_SSLv2
             ctx.options |= ssl.OP_NO_SSLv3
+
+            ctx.load_cert_chain(certfile=SOLEDAD_CERT)
+            ctx.verify_mode = ssl.CERT_REQUIRED
+            self.sock = ctx.wrap_socket(
+                sock, server_side=True, server_hostname=self.host)
+
         except AttributeError:
-            pass
-
-        ctx.load_cert_chain(certfile=SOLEDAD_CERT)
-        ctx.verify_mode = ssl.CERT_REQUIRED
-
-        self.sock = ctx.wrap_socket(
-            sock, server_side=True, server_hostname=self.host)
+            self.sock = ssl.wrap_socket(
+                sock, ca_certs=SOLEDAD_CERT, cert_reqs=ssl.CERT_REQUIRED,
+                ssl_version=highest_supported)
 
         match_hostname(self.sock.getpeercert(), self.host)
 

commit d234ec94734219116b1190232b6ba9c1a118e1d6

Fix incorrect ssl context setup
The changes introduced in aafa79c0f5 having to do with the cert
verification are incorrect, regarding the use of the newest ssl context
api introduced in python 2.7.9. There the use of the server setup was
taken, instead of the correct client options.

I hereby apologize for the insuficient testing on that fix. It happens
that I wrongly tested in an evironment that did the fallback to
pre-2.7.9 interpreter.

client/src/leap/soledad/client/__init__.py
@@ -819,10 +819,9 @@ class VerifiedHTTPSConnection(httplib.HTTPSConnection):
             ctx.options |= ssl.OP_NO_SSLv2
             ctx.options |= ssl.OP_NO_SSLv3
 
-            ctx.load_cert_chain(certfile=SOLEDAD_CERT)
+            ctx.load_verify_locations(cafile=SOLEDAD_CERT)
             ctx.verify_mode = ssl.CERT_REQUIRED
-            self.sock = ctx.wrap_socket(
-                sock, server_side=True, server_hostname=self.host)
+            self.sock = ctx.wrap_socket(sock)
 
         except AttributeError:
             self.sock = ssl.wrap_socket(