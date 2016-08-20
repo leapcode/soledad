fa4bf209bef0ca4fd6145c8d518c3d99f770cb65
Victor Shyba <victor.shyba@gmail.com>
2016-08-20 01:42:13 -0300

    [test] adds tests for raw encryption
    Hypothesis: raw vs doc
    Added the same sizes set (10k, 100k, 500k, 1M, 10M, 50M) as the document
    crypto test, so we can compare how close to raw the higher level
    operation is.

--- testing/tests/perf/test_crypto.py
+++ testing/tests/perf/test_crypto.py
@@ -2,6 +2,8 @@ import pytest
 import json
 from uuid import uuid4
 from leap.soledad.common.document import SoledadDocument
+from leap.soledad.client.crypto import encrypt_sym
+from leap.soledad.client.crypto import decrypt_sym
 
 
 def create_doc_encryption(size):
@@ -46,3 +48,34 @@ test_decrypt_doc_500k = create_doc_decryption(500*1000)
 test_decrypt_doc_1M = create_doc_decryption(1000*1000)
 test_decrypt_doc_10M = create_doc_decryption(10*1000*1000)
 test_decrypt_doc_50M = create_doc_decryption(50*1000*1000)
+
+KEY = 'x'*32
+
+
+def create_raw_encryption(size):
+    @pytest.mark.benchmark(group="test_crypto_raw_encrypt")
+    def test_raw_encrypt(benchmark):
+        benchmark(encrypt_sym, 'x'*size, KEY)
+    return test_raw_encrypt
+
+
+def create_raw_decryption(size):
+    @pytest.mark.benchmark(group="test_crypto_raw_decrypt")
+    def test_raw_decrypt(benchmark):
+        iv, ciphertext = encrypt_sym('x'*size, KEY)
+        benchmark(decrypt_sym, ciphertext, KEY, iv)
+    return test_raw_decrypt
+
+
+test_encrypt_raw_10k = create_raw_encryption(10*1000)
+test_encrypt_raw_100k = create_raw_encryption(100*1000)
+test_encrypt_raw_500k = create_raw_encryption(500*1000)
+test_encrypt_raw_1M = create_raw_encryption(1000*1000)
+test_encrypt_raw_10M = create_raw_encryption(10*1000*1000)
+test_encrypt_raw_50M = create_raw_encryption(50*1000*1000)
+test_decrypt_raw_10k = create_raw_decryption(10*1000)
+test_decrypt_raw_100k = create_raw_decryption(100*1000)
+test_decrypt_raw_500k = create_raw_decryption(500*1000)
+test_decrypt_raw_1M = create_raw_decryption(1000*1000)
+test_decrypt_raw_10M = create_raw_decryption(10*1000*1000)
+test_decrypt_raw_50M = create_raw_decryption(50*1000*1000)
