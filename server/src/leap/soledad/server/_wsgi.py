6793ff931a5fea247d5bd1634d48a789f5e7d845
drebs <drebs@leap.se>
2017-01-22 19:36:42 -0200

    [refactor] rename server auth classes

--- server/src/leap/soledad/server/_wsgi.py
+++ server/src/leap/soledad/server/_wsgi.py
@@ -15,11 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
-A WSGI application to serve as the root resource of the webserver.
-
-Use it like this:
-
-  twistd web --wsgi=leap.soledad.server.application.wsgi_application
+A WSGI application that serves Soledad synchronization.
 """
 from twisted.internet import reactor
 from twisted.python import threadpool
