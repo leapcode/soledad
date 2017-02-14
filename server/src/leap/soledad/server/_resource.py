87bfc0ec7d7faae9dceea3717611a1a2851ad93f
Kali Kaneko <kali@leap.se>
2017-02-15 00:37:04 +0100

    [feature] add robots.txt
    -Resolves: #6178

--- server/src/leap/soledad/server/_resource.py
+++ server/src/leap/soledad/server/_resource.py
@@ -27,21 +27,27 @@ from ._wsgi import get_sync_resource
 __all__ = ['SoledadResource', 'SoledadAnonResource']
 
 
-class Robots(Resource):
+class _Robots(Resource):
     def render_GET(self, request):
-        return 'robots, go away! please!'
+        return (
+            'User-agent: *\n'
+            'Disallow: /\n'
+            '# you are not a robot, are you???')
 
 
 class SoledadAnonResource(Resource):
+
     """
-    The parts of Soledad Server that unauthenticated users can see
+    The parts of Soledad Server that unauthenticated users can see.
+    This is nice because this means that a non-authenticated user will get 404
+    for anything that is not in this minimal resource tree.
     """
 
     def __init__(self, enable_blobs=False):
         Resource.__init__(self)
         server_info = ServerInfo(enable_blobs)
         self.putChild('', server_info)
-        self.putChild('robots.txt', Robots())
+        self.putChild('robots.txt', _Robots())
 
 
 class SoledadResource(Resource):
