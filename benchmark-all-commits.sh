97ca968e622796e5e783b6e4c410bee36df90803
varac <varacanero@zeromail.org>
2017-04-05 12:47:40 +0200

    [benchmarks] Run test in docker

--- benchmark-all-commits.sh
+++ benchmark-all-commits.sh
@@ -18,17 +18,20 @@ while /bin/true
 do
   echo -e '\n\n\n\n\n'
 
-  git checkout origin/master testing/tox.ini testing/tests/benchmarks/conftest.py testing/tests/conftest.py
 
-  curl -s localhost:5984 || exit 1
 
+  # Option 1: Run couchdb + tox localy - dirty!
+  # curl -s localhost:5984 || exit 1
   # Make sure we kill all leftover soledad server/pytest procs
-  pkill -f -9 tox
-  tox -c testing/tox.ini -e benchmark -- -x "$PYTEST_OPTS"
-  #gitlab-runner exec docker benchmark --env PYTEST_OPTS="$PYTEST_OPTS"
+  #pkill -f -9 tox
+  #git checkout origin/master testing/tox.ini testing/tests/benchmarks/conftest.py testing/tests/conftest.py
+  #tox -c testing/tox.ini -e benchmark -- -x "$PYTEST_OPTS"
+  #git reset HEAD testing/tox.ini
+  #git checkout testing/tox.ini testing/tests/benchmarks/conftest.py testing/tests/conftest.py
 
+  # Option 2: Run couchdb + tox in docker container using gitlab-runner
+  git checkout origin/benchmark-all-commits .gitlab-ci.yml
+  gitlab-runner exec docker benchmark --env PYTEST_OPTS="$PYTEST_OPTS" --env HOST_HOSTNAME="$(hostname)"
 
-  git reset HEAD testing/tox.ini
-  git checkout testing/tox.ini testing/tests/benchmarks/conftest.py testing/tests/conftest.py
   git checkout HEAD^
 done
